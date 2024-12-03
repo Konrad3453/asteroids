b8e7773109dba453e7e348640d09b80673f30880
Konrad3453 <konrad.baj98@gmail.com>
2024-12-03 12:37:04 +0100

    added score and life classes

--- main.py
+++ main.py
@@ -5,17 +5,18 @@ from asteroid import Asteroid
 from asteroidfiield import *
 from shot import Shot
 import sys 
-
-
+from score import Score
+from lives import Life
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-
+    lives = Life(pygame.font.SysFont('Arial', 30), SCREEN_WIDTH - 350, 10)
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
     shots = pygame.sprite.Group()
+    score = Score(pygame.font.SysFont('Arial', 30), SCREEN_WIDTH - 150, 10)
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     
@@ -38,16 +39,17 @@ def main():
                 if asteroid.collision(bullet):
                     bullet.kill()
                     asteroid.split()
-                    
+                    score.increase()
 
         for asteroid in asteroids:
             if asteroid.collision(player):
-                print("Game over!")
-                sys.exit()
+                lives.taking_damage()
         screen.fill("black")
        
         for i in drawable:
             i.draw(screen)
+        score.draw(screen)
+        lives.draw(screen)
         pygame.display.flip()
         # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
